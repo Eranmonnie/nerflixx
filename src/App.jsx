c9f76c91548b7fad703b5ce8edb5d53056d131af
Eranmonnie <ajalaferanmi123@gmail.com>
2023-11-23 20:29:18 +0100

    installed netflix fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 const App = () => {
   return ( 
-    <div className="text-5xl text-red-500">App</div>
+    <div className="uppercase">
+      <h1 className="font-nsans-light">netflix</h1>
+      <h1 className="font-nsans-bold">netflix</h1>
+      <h1 className="font-nsans-regular">netflix</h1>
+      <h1 className="font-nsans-medium">netflix</h1>
+      
+    </div>
    );
 }
  
-export default App;
+export default App;  
