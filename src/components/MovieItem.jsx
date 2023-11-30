dd2557ef23b19a91d01a2f56215008374f07950c
Eranmonnie <ajalaferanmi123@gmail.com>
2023-11-30 22:45:07 +0100

    poster images incase backdrop path isnt available

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { createMovieImage } from '../services/movieServices'
 
 export const MovieItem = ({movie}) => {
-   const {backdrop_path, title} = movie
+   const {backdrop_path, title, poster_path} = movie
   return (
     <div className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2">
-        <img src={createMovieImage(backdrop_path, "w500")} alt={title} />
+        <img
+        className="w-full h-40 block object-cover"
+         src={createMovieImage(backdrop_path ?? poster_path, "w500")} alt={title} />
     </div>
   )
 }
